a70a7739e207ce44750d0f2c19af740495f184ad
Jose Edmundo Martinez Retama <jmartinez@lamxp-ewks20105.aceins.com>
2019-10-12 10:20:28 -0400

    added the slider to select the storage

--- src/product/ProductsContainer.tsx
+++ src/product/ProductsContainer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Grid} from '@material-ui/core';
+import {Grid, Slider} from '@material-ui/core';
 import Product from './Product';
 
 //const useStyles = makeStyles();
@@ -7,14 +7,50 @@ import Product from './Product';
 export default function ProductsContainer() {
   //const classes = useStyles(props);
 
+  //TODO: switch this logic to calculate positions based on criteria
+  const marks = [
+    {
+      value: 0,
+      label: '32GB',
+    },
+    {
+      value: 50,
+      label: '64GB',
+    },
+    {
+      value: 100,
+      label: '128GB',
+    },
+  ];
+  
+  function valuetext(value:any) {
+    return `${value}°C`;
+  }
+  
+  function valueLabelFormat(value: any) {
+    return marks.findIndex(mark => mark.value === value) + 1;
+  }
+
   //todo mad an iteration for this
   return (
-    <Grid container={true} spacing={2}>
-        <Product />
+    <>
+      <Slider
+      defaultValue={20}
+      valueLabelFormat={valueLabelFormat}
+      getAriaValueText={valuetext}
+      aria-labelledby="discrete-slider-restrict"
+      step={null}
+      valueLabelDisplay="auto"
+      marks={marks}
+      />
+      <Grid container={true} spacing={2}>
+          <Product />
 
-        <Product />
+          <Product />
 
-        <Product />
-    </Grid>
+          <Product />
+      </Grid>
+      
+    </>
   );
 }
