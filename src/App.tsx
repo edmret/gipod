510d2073fc420cb1302ca0bb8c2f459b3e0ea8b5
Jose Edmundo Martinez Retama <jmartinez@laprp-elpt10806.aceins.com>
2019-10-13 12:18:39 -0400

    implemented a way to obtain device information FAKED

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,26 @@ import { StateRouterRender } from './_stateRouter/routerRender';
 
 import {PhoneInfo} from './UserInfo/PhoneInfo';
 
+
+//fake and hardcoded
+window['widgetChubbDevices'] = {
+  freestorage: 18892,
+  imei: "354263090723715",
+  loc: {
+    address: "9209 S Dadeland Blvd, Kendall, FL 33156, USA",
+    city: "Miami-Dade County",
+    country: "US",
+    county: "Kendall",
+    lat: 25.6853542
+  },
+  memory: "5579.45",
+  platform: "Android",
+  totalstorage: 64000,
+  uuid: "633565e9eadec2b5",
+};
+
+
+
 const initialState:DonWidgetState = {
   imeiDevice: {},
   currentRoute: 'device-info',
@@ -23,9 +43,7 @@ const initialState:DonWidgetState = {
     Phone: '',
     BirthDate: ''
   },
-  deviceInfo: {
-    
-  }
+  deviceInfo: window['widgetChubbDevices'] || {}
 };
 
 
